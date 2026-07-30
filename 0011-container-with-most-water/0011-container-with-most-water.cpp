5e972f3dff683cf90fd99de5011d4f388119de07
raktimneog08-ops <raktimneog08@gmail.com>
2026-07-30 22:47:24 +0530

    Time: 2 ms (38.44%), Space: 63 MB (11.28%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int maxArea(std::vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int max_water = 0;
+        
+        while (left < right) {
+            int current_height = std::min(height[left], height[right]);
+            int width = right - left;
+            int current_water = current_height * width;
+            
+            max_water = std::max(max_water, current_water);
+            
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return max_water;
+    }
+};
