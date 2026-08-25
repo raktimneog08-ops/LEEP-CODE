f94ddb9354a97f31f50652481ae1bc4074691de8
RAKTIM NEOG <raktimneog08@gmail.com>
2026-08-25 22:30:58 +0530

    Time: 6 ms (96.33%), Space: 57.2 MB (71.26%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+
+        // Find the middle of the list using slow and fast pointers
+        ListNode* prev = nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Disconnect the two halves
+        prev->next = nullptr;
+
+        // Recursively sort each half
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(slow);
+
+        // Merge sorted halves
+        return merge(left, right);
+    }
+};
